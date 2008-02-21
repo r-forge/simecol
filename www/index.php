78a50d6e4f6fe735a434144af42ec32e27823051
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2008-02-21 16:20:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/simecol@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -42,8 +42,198 @@ fclose($handle);
 echo $contents; } ?>
 
 <!-- end of project description -->
+<!-- thpe: begin of user supplied material -->
 
-<p> No content added. </p>
+<link rel="stylesheet" href="style.css">
+  
+<style type="text/css">
+tr {background: #FFFFCC}
+ul {font-size: 10pt}
+</style>
+
+<h1>Ecological modelling with R and the <span style="color: rgb(153, 0, 0);">simecol</span>
+package<a href="http://www.r-project.org/"><img style="border: 0px solid ; width: 100px; height: 76px; float: right;" alt="The R Project of Statistical Computing" src="Rlogo.jpg"></a></h1>
+<h2>What is <span style="color: rgb(153, 0, 0);">simecol</span>?<span style="font-family: monospace; font-weight: bold; color: rgb(153, 0, 0);"></span></h2>
+<p><span style="font-family: monospace; font-weight: bold; color: rgb(153, 0, 0);"><span style="font-family: sans-serif;">simecol</span></span> (<span style="color: rgb(153, 0, 0); font-weight: bold;">sim</span>ulation of <span style="color: rgb(153, 0, 0); font-weight: bold;">ecol</span>ogical systems) is an <span style="font-weight: bold; color: rgb(51, 51, 153);">R</span>
+package which is based on an object oriented programming paradigm for the implementation of ecological simulation models.</p>
+<p>
+The <code style="color: rgb(153, 0, 0);">simecol</code> package is intended to give users
+(students and scientists) an interactive environment to implement,
+distribute, simulate and document basic and advanced ecological models
+without the
+need to write long simulation programs. For this purpose, an object
+oriented approach is developed, which should provide a consistent
+but still
+flexible and extensible way to implement simulation models of different
+types, namely
+</p>
+<ul>
+  <li>ordinary differential equation (ODE) models,
+  </li>
+  <li>non-spatial individual-based models,
+  </li>
+  <li>grid-oriented individual-based models,
+  </li>
+  <li>particle diffusion-type models</li>
+  <li>and more.<br>
+    </li>
+</ul>
+<p>
+Each simulation model is implemented as <code style="color: rgb(153, 0, 0);">simecol</code><span style="color: rgb(153, 0, 0);">
+</span>simulation model object with components <code style="color: rgb(153, 0, 0);">main</code><span style="color: rgb(153, 0, 0);">,</span> holding the main model
+equations, rules or arbitrary program code <span style="font-family: monospace; color: rgb(153, 0, 0);">equations</span> (optional, a list of possibly nested sub-models or sub-equations ), <code style="color: rgb(153, 0, 0);">parms</code><span style="color: rgb(153, 0, 0);"> </span>with model
+parameters,<span style="font-family: monospace; color: rgb(153, 0, 0);"> init</span> with the initial state, <span style="font-family: monospace; color: rgb(153, 0, 0);">inputs</span> (optional) for external input data and <code style="color: rgb(153, 0, 0);">times</code><span style="color: rgb(153, 0, 0);"> </span>to define the
+simulation time and the time steps used.
+</p>
+<br>
+<h2>Prerequisites</h2>
+<ul>
+  <li><span style="font-weight: bold; color: rgb(153, 0, 0); font-family: monospace;">simecol</span>
+is based on <span style="font-weight: bold; color: rgb(51, 51, 153);">R</span>,
+a freely available system for statistical computation
+and graphics. Current versions require <a style="font-weight: bold;" href="http://cran.r-project.org">R
+version
+2.30</a> or above.</li>
+  <li>simecol depends on the <span style="color: rgb(153, 0, 0); font-family: monospace; font-weight: bold;">odesolve</span> package for numerical integration.</li>
+  <li>optional: the <span style="color: rgb(153, 0, 0); font-family: monospace; font-weight: bold;">tcltk</span> package is required for the graphical parameter editing functions </li>
+  <li>Operating systems: All operating systems on which <span style="font-weight: bold; color: rgb(51, 51, 153);">R</span>
+can be
+installed: Linux, Windows, Mac, different UNIXes </li>
+</ul>
+<ul>
+</ul>
+<h2><img style="width: 200px; height: 200px; float: right;" alt="A simple stochastic cellular automaton" src="cc2.gif">Current State</h2>
+<p>The <span style="font-weight: bold; color: rgb(153, 0, 0); font-family: monospace;">simecol</span> package, that is based on an object oriented programming (OOP)  is available from <a href="http://cran.r-project.org">CRAN</a>
+and most of its mirrors. The general OOP model description is,
+mostly independent of a particular object system, so the general object
+model may be useful useful also <span style="font-weight: bold; color: rgb(153, 0, 0); font-family: monospace;"></span>with <span style="font-weight: bold; color: rgb(51, 51, 153);">R's</span> alternative object systems (<a href="http://www.maths.lth.se/help/R/R.oo/" target="_blank">R.oo</a> and <a href="../proto/">proto</a>, see useR-2006 poster below for details).</p>
+<h2>Installation</h2>
+<p><span style="font-weight: bold; color: rgb(153, 0, 0);"></span><span style="font-weight: bold; color: rgb(153, 0, 0); font-family: monospace;">simecol</span> can be installed directly from the internet within <span style="font-weight: bold; color: rgb(51, 51, 153);">R</span>
+either via
+the menu (on Windows) or via the <span style="font-weight: bold; color: rgb(51, 51, 153);">R</span>
+command line:</p>
+<br>
+<table border="1" cellpadding="4" cellspacing="0">
+  <tbody>
+    <tr>
+      <td>
+      <p><span style="font-family: monospace;">&gt;install.packages("simecol")</span><br>
+      </p>
+      </td>
+    </tr>
+  </tbody>
+</table>
+<br>
+<p>
+The source code and precompiled packages for Windows and Mac are available from <a href="http://cran.r-project.org/src/contrib/Descriptions/simecol.html">CRAN</a> in
+*.tar.gz resp. *.zip or *.tgz format. Access to
+the simecol SVN
+repository may be enabled upon <a href="mailto:petzoldt@rcs.urz.tu-dresden.de">request</a>.
+</p>
+<h2>Documentation</h2>
+<h3>Reference Articles</h3>
+<ul>
+  <li>Petzoldt, T. and K. Rinke (2007).  simecol: An Object-Oriented Framework for Ecological
+  Modeling in R.  Journal of Statistical  Software, 22(9), 1--31.  URL http://www.jstatsoft.org/v22/i09 <a href="http://www.jstatsoft.org/v22/i09">(pdf)</a>.</li>
+  <li>Petzoldt, T. (2003). R as a Simulation Platform in Ecological Modelling. R-News 3(3), 8--16. <a href="http://cran.r-project.org/doc/Rnews/Rnews_2003-3.pdf">(pdf)</a></li>
+</ul>
+<h3>Additional Material</h3>
+<ul>
+  <li><a href="http://cran.r-project.org/doc/packages/simecol.pdf">Online documentation</a> of the package (functions, examples).</li>
+  <li><a href="http://hhbio.wasser.tu-dresden.de/projects/simecol/useR-2004.pdf">Lecture slides</a> from the useR-2004 conference about the <span style="font-weight: bold; color: rgb(153, 0, 0); font-family: monospace;">simecol</span> concept and the (old) S3 list-based approach.<br>
+  </li>
+  <li><a href="http://www.tu-dresden.de/fghhihb/petzoldt/modlim/modlim.pdf">German tutorial</a> describing ecological modelling with <span style="font-weight: bold; color: rgb(51, 51, 153);">R</span> in general (a little bit outdated and still without <span style="font-weight: bold; color: rgb(153, 0, 0); font-family: monospace;">simecol</span>).</li>
+  <li><a href="http://hhbio.wasser.tu-dresden.de/projects/simecol/useR-2006-poster.pdf">Poster</a> and <a href="useR2006-slides">lecture slides</a> of the <a href="http://www.r-project.org/useR-2006/">useR-2006</a> conference comparing OOP approaches and introducing <span style="font-weight: bold; color: rgb(153, 0, 0); font-family: monospace;">simecol</span><span style="font-weight: bold; color: rgb(153, 0, 0);"></span>.</li>
+</ul>
+<h3>Links</h3>
+<ul>
+  <li>The <a href="http://www.r-project.org/">R
+project</a> of statistical computing</li>
+  <li><a href="http://cran.r-project.org/src/contrib/Descriptions/simecol.html">Download</a> latest version of simecol</li>
+  <li><a href="http://tu-dresden.de/Members/thomas.petzoldt">Thomas
+Petzoldt</a>'s homepage</li>
+</ul>
+<h2>Examples<span style="color: rgb(192, 192, 192);"></span></h2>
+<table style="text-align: left; width: 100%;" border="1" cellpadding="4" cellspacing="0">
+  <tbody>
+    <tr>
+      <td style="width: 30%; vertical-align: top; font-weight: bold;">
+      <p>A basic Lotka-Volterra model</p>
+      </td>
+      <td style="width: 79%;"><span style="font-family: monospace;">library("simecol")</span><br style="font-family: monospace;">
+      <span style="font-family: monospace;">data(lv, package="simecol")</span><br style="font-family: monospace;">
+      <span style="font-family: monospace;">plot(sim(lv))</span></td>
+    </tr>
+    <tr>
+      <td style="vertical-align: top;">
+      <p><span style="font-weight: bold;">The classical Conway's Game of Life</span></p>
+      </td>
+      <td><span style="font-family: monospace;"></span><span style="font-family: monospace;">library("simecol")<br>
+data(conway</span><span style="font-family: monospace;">, package="simecol"</span><span style="font-family: monospace;">)<br>
+plot(sim(conway))<br>
+m &lt;- matrix(0, 40, 40)<br>
+m[5:35,19:21] &lt;-1<br>
+init(conway) &lt;- m<br>
+sim(conway, animate=TRUE, delay=100, col=c("white", "green"), axes=FALSE)</span><br style="font-family: monospace;">
+      </td>
+    </tr>
+    <tr>
+      <td style="vertical-align: top;">
+      <p style="font-weight: bold;">Object oriented (S4 based) representation of a simulation model object (simecol object).</p>
+      <p><br>
+      </p>
+      <p><br>
+      </p>
+      </td>
+      <td><span style="font-family: monospace; color: rgb(204, 0, 0);"></span><span style="font-family: monospace;"><span style="color: rgb(204, 0, 0);">
+library("simecol") # load the package first!</span><br>
+      <br>
+      </span><span style="font-family: monospace;">conway &lt;- new("gridModel",<br>
+&nbsp;&nbsp;&nbsp; main = function(time, init, parms) {<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x&nbsp;&nbsp; &lt;- init<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; srv &lt;- parms$srv<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; gen &lt;- parms$gen<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; n&nbsp;&nbsp; &lt;- nrow(x)<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; m&nbsp;&nbsp; &lt;- ncol(x)<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; nb&nbsp; &lt;- eightneighbours(x)<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ## survival rule <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; xsrv &lt;- ifelse(x &gt; 0 &amp; (nb %in% srv), 1, 0)<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ## generation rule<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; xgen &lt;- ifelse(x == 0 &amp; (nb %in% gen), 1, 0)<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x&nbsp;&nbsp;&nbsp; &lt;- as.numeric((xgen + xsrv)&gt;0)<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; dim(x) &lt;- c(n,m)<br>
+&nbsp;&nbsp;&nbsp; x<br>
+&nbsp;&nbsp;&nbsp; },<br>
+&nbsp;&nbsp;&nbsp; parms = list(srv=c(2, 3), gen=3),<br>
+&nbsp;&nbsp;&nbsp; times = c(from=1, to=10, by=1),<br>
+&nbsp;&nbsp;&nbsp; init = matrix(round(runif(40*40)), nrow=40, ncol=40),<br>
+&nbsp;&nbsp;&nbsp; solver = "iteration"<br>
+)<br>
+      <br>
+## and to run this example:<br>
+      <br>
+plot(sim(conway))</span></td>
+    </tr>
+  
+  </tbody>
+</table>
+<h2>License</h2>
+<p><span style="color: rgb(153, 0, 0); font-family: monospace; font-weight: bold;">simecol</span>,
+like <span style="font-weight: bold; color: rgb(51, 51, 153);">R</span>,
+is free open source software licensed under the
+<a href="COPYING" target="_blank">GNU Public License</a>
+(GPL 2.0 or above).</p>
+<span style="font-weight: bold;">NO WARRANTY:</span>
+The software is provided as is, according to the <a href="http://hhbio.wasser.tu-dresden.de/projects/simecol/COPYING" target="_blank">GPL</a>.
+
+<h2>Authors</h2>
+<p>Thomas Petzoldt</p>
+<p>2008-02-21, feedback and comments are <a href="mailto:thomas.petzoldt@tu-dresden.de">welcome</a>.<br>
+</p>
+<p><a href="http://tu-dresden.de/Members/thomas.petzoldt">to ThPe's homepage</a></p>
+
+
+<!-- thpe: end of user supplied material -->
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
