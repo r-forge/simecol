4e23f24f883626c305c7bbb7031ecec904faf92d
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2009-02-08 11:03:51 +0000

    rounding of model parameters consistent to paper; dummy function for epsilon()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/simecol@115 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/simecolModels/R/dvmphyto.R
+++ pkg/simecolModels/R/dvmphyto.R
@@ -9,7 +9,6 @@
 ################################################################################
 
 dvm_phyto <- function() {
-
   new("odeModel",
     main = function(time, init, parms) {
       X <- init[1:2]
@@ -83,32 +82,35 @@ dvm_phyto <- function() {
     init = c(Xr = 0.05, Xk = 0.05, Z = 0.05, P = 30), # in mg/L, P in \mug/L
     solver = "lsoda",
     parms = list(
-      DVM           = FALSE,
+      DVM           = FALSE,             # pulsed grazing (FALSE or TRUE)
       a             = 6,                 # Time of sunrise (6 h)
       ae            = 0.7,               # Assimilation efficency (0.7, nondimensional)
       b             = 16,                # Length of day (16 h)  -
       d             = 1e-3,              # Gloaming constant	(10^-3, nondimensional)
       epsilon_min   = 0.6,               # Background turbidity (0.6 m^-1)
       epsilon_X     = 0.5,               # Light absorption by phytoplankton (0.5 m^-1 mg,C^-1 l)
-      I_komp        = 5 / 8.64,          # Light threshold for algal growth (0.58 W m^-2)
+      I_komp        = 0.58,              # Light threshold for algal growth (0.58 W m^-2)
       I_max         = 560,               # Maximum value of irradiation  (560  W m^-2)
       I_phot        = 0.5,               # Ratio of photosynthetic active radiation of light (0.5, nondimensional)
       I_ref         = 0.1,               # Reflexion of light at water surface (0.1, nondimensional)
-      imp_P         = 0.137 / 24,        # P import (0.5 g m^-2 a^-1)
+      imp_P         = 0.5*1e3/10/365/24, # P import (0.5 g m^-2 a^-1)
       mort_max      = 0.045/24,          # Maximal mortality of Z (0.045 d^-1)
-      k_I           = 29/8.64,           # Half saturation coefficient light (3.36 W m^-2)
+      k_I           = 3.36,              # Half saturation coefficient light (3.36 W m^-2)
       k_ing         = 0.164,             # Half saturation coefficient ingestion (0.164  mg,C,l^-1)
-      k_mort        = 0.0158,            # Half saturation coefficient mortality of Z (0.016 mg,C,l^-1)
+      k_mort        = 0.016,             # Half saturation coefficient mortality of Z (0.016 mg,C,l^-1)
       k_P           = 5,                 # Half saturation coefficient phosphorus (5 mu ,g,P,l^-1)
-      resz_max      = 1/24 * 0.288 * (2.66 * 1.5^3.09)^-0.15,  # Maximum respiration for Z (0.21 d^-1)
+      resz_max      = 0.21/24,           # Maximum respiration for Z (0.21 d^-1)
       temp          = 20,                # Temperature (20 degrees C)
       v_sed         = 0.1/24,            # Sinking velocity of phytoplankton (0.1 m d^-1)
       yield_CP      = 24.4,              # ratio of carbon to phosphorus (24.4 mgC (mu gP)^-1)
       yield_fae     = 0.7,               # ratio of resolved phosphorus from feaces (0.7, nondimensional)
       z_mix         = 10,                # Depth of epilimnion (10  m)
-      phot_max_i    = c(2.5, 1) /24,     # Maximum growth rate for X_i (1.5, 0,6 d^-1)
+      phot_max_i    = c(2.5, 1) /24,     # Maximum growth rate for X_i (2.5, 1.0 d^-1)
       imp_X_i       = rep(0.0005, 2)/24, # Import of algal biomass (5e-04, 5e-04  mg,C,l^-1)
-      ing_max_i     = rep(0.392/(2.66 * 1.5^3.09), 2) / c(1, 5)  # Maximal ingestion of phytoplankton group i  (1.0, 0.2  d^-1)
+      ing_max_i     = c(1, .2)/24        # Maximal ingestion of phytoplankton group i  (1.0, 0.2  d^-1)
     )
   )
 }
+
+## global dummy function
+epsilon <- function(...) stop("function 'epsilon' not correctly assigned")
