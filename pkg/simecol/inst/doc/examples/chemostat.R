fc52f5790f1e7046fddc6af97e9604527a5847e0
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2011-02-03 21:36:18 +0000

    update chemostat model
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/simecol@187 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/simecol/inst/doc/examples/chemostat.R
+++ pkg/simecol/inst/doc/examples/chemostat.R
@@ -1,27 +1,20 @@
 chemostat <- new("odeModel",
-    main = function(time, init, parms, inputs=NULL) {
-        vm  <- parms["vm"] # max growth rate 
-        km  <- parms["km"] # half saturation constant
-        D   <- parms["D"]  # dilution rate
-        S0  <- parms["S0"] # substrate in inflow
-        Y   <- parms["Y"]  # yield coefficient for substrate
-        X   <- init[1]     # cells (e.g. algae)
-        S   <- init[2]     # substrate (e.g. phosphorus)
-
-        mu  <- vm * S/(km + S)              # Monod equation
-        dx1 <- mu * X - D * X               # cells 
-        dx2 <-  D *(S0 - S) - 1/Y * mu * X  # substrate
-        list(c(dx1, dx2))
-    },
-    parms = c(
-        vm = 1.0,           # 1/d
-        km = 2.0,           # mumol Substrate/l
-        Y  = 100,           # cells /mumol Substrate
-        D  = 0.5,           # 1/d
-        S0 = 10             #  mumol Substrate/l
-    ),
-    times = c(from=0, to=40, by=.5),
-    init  = c(X=10, S=10), # cells/l; umol Substrate/l                     
-    solver = "lsoda"
+  main = function(time, init, parms, inputs = NULL) {
+    with(as.list(c(init, parms)), {
+      mu  <- vm * S/(km + S)              # Monod equation
+      dx1 <- mu * X - D * X               # cells, e.g. algae
+      dx2 <-  D *(S0 - S) - 1/Y * mu * X  # substrate, e.g. phosphorus
+      list(c(dx1, dx2))
+    })
+  },
+  parms = c(
+    vm = 1.0,           # max growth rate, 1/d
+    km = 2.0,           # half saturation constant, mumol / L
+    Y  = 100,           # cells /mumol Substrate
+    D  = 0.5,           # dilution rate, 1/d
+    S0 = 10             # substrate in inflow, mumol / L
+  ),
+  times = c(from=0, to=40, by=.5),
+  init  = c(X=10, S=10), # cells / L; Substrate umol / L
+  solver = "lsoda"
 )
-
