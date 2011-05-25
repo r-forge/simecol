1092783c7bc6daf1d036400e5b57116046029849
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2011-05-25 06:40:57 +0000

    NEWS
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/simecol@206 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/simecol/inst/doc/examples/lv3_approxfun.R
+++ pkg/simecol/inst/doc/examples/lv3_approxfun.R
@@ -6,11 +6,11 @@ lv3 <- new("odeModel",
       ds <- s.in  - b*s*p + g*k
       dp <- c*s*p - d*k*p
       dk <- e*p*k - f*k
-      list(c(ds, dp, dk), s.in)
+      list(c(ds, dp, dk), s.in = s.in)
     })
   },
   parms = c(b = 0.1, c = 0.1, d = 0.1, e = 0.1, f = 0.1, g = 0),
-  times  = c(from = 0, to = 200, by = 1),
+  times  = c(from = 0, to = 200, by = .1),
   inputs = approxfun(
     c(0,   99, 100,  101, 200), # time
     c(0.1, 0.1, 0.5, 0.1, 0.1), # s.in
