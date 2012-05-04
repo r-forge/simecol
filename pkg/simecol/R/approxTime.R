020dddb6019e6243e01e7e6e9349aea9fc8c7b02
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2012-05-04 09:22:10 +0000

    tweaks for approxTime1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/simecol@224 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/simecol/R/approxTime.R
+++ pkg/simecol/R/approxTime.R
@@ -15,27 +15,3 @@ approxTime <- function(x, xout, ...) {
   names(y) <- dimnames(x)[[2]]
   y
 }
-
-approxTime1 <- function(x, xout, rule=1) {
-  # first colum:   x (independend values, e.g. time)
-  # other columns: y (dependend data)
-  # ... error checking for dataframe or matrix etc ...
-  if (!is.matrix(x)) x <- as.matrix(x)
-  n <- nrow(x)
-  if (xout >= x[n,1]) {
-    y <- x[n,]
-    if (rule==1 & (xout > x[n+1])) y[2:length(y)] <- NA
-  } else if (xout <= x[1,1]) {
-      y <- x[1,]
-      if (rule==1 & (xout < x[1])) y[2:length(y)] <- NA
-  } else {
-      i  <- which.max(x[,1] > xout)
-      x1 <- x[i-1,1]
-      x2 <- x[i,1]
-      y1 <- x[i-1,]
-      y2 <- x[i,]
-      y  <- y1 + (y2 - y1) * (xout - x1) / (x2 - x1)
-  }
-  names(y) <-  dimnames(x)[[2]]
-  y
-}
